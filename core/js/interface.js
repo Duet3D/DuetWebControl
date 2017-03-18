27a73c645681af9a14d2cedb893f9b6f35d97cd2
Jackal <jackal.c@gmail.com>
2017-03-18 12:54:58 +0800

    Update manual webcam refresh

--- core/js/interface.js
+++ core/js/interface.js
@@ -394,7 +394,7 @@ $("body").on("hidden.bs.popover", function() {
 	$(this).popover("destroy");
 });
 
-$("#img_webcam").click(function(){$(this).attr("src",$(this).attr("src"))});
+$("#img_webcam").click(function(){updateWebcam(true)});
 
 /* Static GUI Events */
 
