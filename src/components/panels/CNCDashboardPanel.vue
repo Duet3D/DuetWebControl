4b8f21f6505657fb7d1c3ff11a4074de9779eb9a
Sindarius <pyroclasmx@yahoo.com>
2020-11-28 16:43:39 -0600

    Add additional controls to CNC Dashboard.

--- src/components/panels/CNCDashboardPanel.vue
+++ src/components/panels/CNCDashboardPanel.vue
@@ -1,14 +1,25 @@
 <template>
-	<v-row >
-		<v-col cols="12" class="pt-0 pb-0" >
-			<cnc-movement-panel class="mb-2"></cnc-movement-panel>
-		</v-col>
-			<v-row>
-				<v-col sm="12" :md="!isFFForUnset && atxPower !== null ? 9 : 12" :lg="!isFFForUnset && atxPower !== null ? 9 : 12" :xl="!isFFForUnset && atxPower !== null ? 10 : 12">
-					<fan-panel></fan-panel>
-				</v-col>
-			</v-row>
-	</v-row>
+	<div>
+		<v-row>
+			<v-col cols="12" class="pt-0 pb-0">
+				<cnc-movement-panel class="mb-2"></cnc-movement-panel>
+			</v-col>
+			<v-col cols="12">
+				<job-progress></job-progress>
+			</v-col>
+		</v-row>
+		<v-row>
+			<v-col cols="4" class="flex-grow-1">
+				<job-control-panel></job-control-panel>
+			</v-col>
+			<v-col cols="4" class="flex-grow-1">
+				<z-babystep-panel></z-babystep-panel>
+			</v-col>
+			<v-col cols="4" class="flex-grow-1">
+				<speed-factor-panel></speed-factor-panel>
+			</v-col>
+		</v-row>
+	</div>
 </template>
 
 <script>
