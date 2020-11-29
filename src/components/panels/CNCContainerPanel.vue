af66280c3f4c113b6024041d21b60ea239711f9d
Sindarius <pyroclasmx@yahoo.com>
2020-11-29 07:33:54 -0600

    Update grid styling on container panel

--- src/components/panels/CNCContainerPanel.vue
+++ src/components/panels/CNCContainerPanel.vue
@@ -7,24 +7,24 @@
 
 <template>
 	<div>
-		<v-row align="stretch" >
-			<v-col cols="2" >
-				<v-card  class="justify-center fill-height">
+		<v-row align="stretch">
+			<v-col cols="3" order="1" sm="4" lg="2" order-lg="1">
+				<v-card class="justify-center fill-height">
 					<v-card-title>
 						<strong>Status</strong>
 					</v-card-title>
-					<v-card-text >
-						<status-label v-if="status" ></status-label>
+					<v-card-text>
+						<status-label v-if="status"></status-label>
 					</v-card-text>
 				</v-card>
 			</v-col>
-			<v-col cols="3">
-				<cnc-axes-position :machinePosition="true"></cnc-axes-position>
+			<v-col cols="6" order="4" sm="6" lg="3" order-lg="2">
+				<cnc-axes-position class="fill-height" :machinePosition="true"></cnc-axes-position>
 			</v-col>
-			<v-col cols="3" class="fill-height">
-				<cnc-axes-position :machinePosition="false"></cnc-axes-position>
+			<v-col cols="6" order="5" sm="6" lg="3" order-lg="3">
+				<cnc-axes-position class="fill-height" :machinePosition="false"></cnc-axes-position>
 			</v-col>
-			<v-col cols="2">
+			<v-col cols="5" order="2" sm="4" lg="2" order-lg="4">
 				<v-card class="fill-height">
 					<v-card-title><strong>Requested Speed</strong></v-card-title>
 					<v-card-text>
@@ -32,7 +32,7 @@
 					</v-card-text>
 				</v-card>
 			</v-col>
-			<v-col cols="2" >
+			<v-col cols="4" order="3" sm="4" lg="2" order-lg="5">
 				<v-card class="fill-height">
 					<v-card-title><strong>Top Speed</strong></v-card-title>
 					<v-card-text>
