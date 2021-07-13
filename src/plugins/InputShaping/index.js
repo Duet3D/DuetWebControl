9be4c1ee764d3a3a31bd89fe7f17efd6d482990d
Michael Szafranek <faule.aepfel@gmail.com>
2021-07-13 17:29:08 +0200

    plugin/InputShaping: add minor fixes
    - removed accelerometer configuration
    - fixed top margins
    - fixed state handling
    - changed parameter of M956

--- src/plugins/InputShaping/index.js
+++ src/plugins/InputShaping/index.js
@@ -7,7 +7,7 @@ import InputShaping from './InputShaping.vue'
 registerRoute(InputShaping, {
 	Settings: {
 		InputShaping: {
-			icon: 'mdi-file-tree',
+			icon: 'mdi-motion-play-outline',
 			caption: 'plugins.inputShaping.menuCaption',
 			path: '/InputShaping'
 		}
