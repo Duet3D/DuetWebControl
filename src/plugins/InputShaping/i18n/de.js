cda665e5284fdd88fffcb48363954ceb0ca2a11d
Michael Szafranek <faule.aepfel@gmail.com>
2021-08-10 07:06:44 +0200

    plugin/InputShaping: moved internationalisation into plugin

---
+++ src/plugins/InputShaping/i18n/de.js
@@ -0,0 +1,26 @@
+export default {
+	name: 'Beschleunigungsmesser',
+	listTitle: 'CSV-Dateien',
+	none: 'Keine Dateien',
+	chartCaption: 'Beschleunigungsproben',
+	noData: 'Keine Proben geladen',
+	analysis: 'Frequenzanalyse',
+	samplingRate: 'Abtastrate (in Hz)',
+	start: 'Start',
+	end: 'Ende',
+	wideBand: 'Breitbandanalyse',
+	analyze: 'Analysieren',
+	back: 'Zurück',
+	overflowPrompt: {
+		title: 'Überlaufe erkannt',
+		prompt: 'Diese Datei hat bereichtet, dass es zu Überlaufen gekommen ist. Sind Sie sicher, dass Sie fortfahren möchten?'
+	},
+	loadError: 'Konnte CSV-Datei nicht laden',
+	parseError: 'Konnte CSV-Datei nicht parsen',
+	frequency: 'Frequenz (in Hz)',
+	amplitudes: 'Amplituden',
+	samples: 'Proben',
+	accelerations: 'Beschleunigungen (in g)',
+	sampleTooltip: 'Probe #{0}',
+	frequencyTooltip: '{0} ± {1} Hz'
+}
--- src/plugins/InputShaping/index.js
+++ src/plugins/InputShaping/index.js
@@ -1,8 +1,14 @@
 'use strict'
 
+import { registerPluginLocalization } from '../../i18n'
 import { registerRoute } from '../../routes'
 
+import de from './i18n/de.js'
+import en from './i18n/en.js'
 import InputShaping from './InputShaping.vue'
+
+registerPluginLocalization('inputShaping', 'de', de);
+registerPluginLocalization('inputShaping', 'en', en);
 
 registerRoute(InputShaping, {
 	Settings: {
@@ -13,3 +19,4 @@ registerRoute(InputShaping, {
 		}
 	}
 });
+
