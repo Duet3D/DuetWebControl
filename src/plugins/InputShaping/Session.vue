574b58f4016420dcf95795ee07c20c206b38afa5
Michael Szafranek <faule.aepfel@gmail.com>
2021-09-07 08:08:59 +0200

    InputShaping: added component to setup session

---
+++ src/plugins/InputShaping/Session.vue
@@ -0,0 +1,93 @@
+<style scoped>
+</style>
+
+<template>
+	<div>
+		Session Component
+
+		<v-row v-if="session">
+			<v-col>
+				name: {{ session.name }} id: {{ session.id }} date: {{ session.date }}<br>
+				test: {{ test.testCommand }}
+			</v-col>
+		</v-row>
+		<v-form ref="formSession" @submit.prevent="submit">
+			<v-row>
+				<v-col>
+					<v-text-field
+						:label="$t('plugins.inputShaping.id')"
+						v-model="session.id"
+						required
+					></v-text-field>
+				</v-col>
+			</v-row>
+			<v-row>
+				<v-col>
+					<v-text-field
+						:label="$t('plugins.inputShaping.name')"
+						v-model="session.name"
+						required
+					></v-text-field>
+				</v-col>
+			</v-row>
+			<v-row>
+				<v-col>
+					<v-text-field
+						:label="$t('plugins.inputShaping.date')"
+						v-model="session.date"
+						required
+					></v-text-field>
+				</v-col>
+			</v-row>
+		</v-form>
+		<test-command
+			v-model="test.testCommand"
+			v-bind:value="session.id"
+		></test-command>
+	</div>
+</template>
+
+<script>
+
+'use strict';
+
+//import { mapState, mapActions } from 'vuex';
+
+//import { AccelStates } from './InputShapingEnums.js';
+import { Session } from './InputShapingSession.js';
+//import { makeNotification } from '../../utils/toast.js';
+
+export default {
+	props: [ ],
+	data() {
+		return {
+			session: new Session(),
+			test: {
+				testCommand: null,
+
+				axis: null,
+				accel: null,
+				param: {
+					numSamples: 1000,			// number of samples to collect by M956
+
+					// axis specific parameters (watch if calibration axis changed if so, update these fields)
+					maxAccel: 0,				// prefill machine->move->axis->AXIS->acceleration
+					maxSpeed: 0,				// prefill machine->move->axis->AXIS->speed (in mm/s)
+					minPosition: 0,				// prefill machine->move->axis->AXIS->min
+					maxPosition: 0,				// prefill machine->move->axis->AXIS->max
+					startPosition: 0,			// prefill maxPosition * 4 / 10
+					stopPosition: 0				// prefill maxPosition * 6 / 10
+				}
+			}
+		};
+	},
+	computed: {
+	},
+	methods: {
+	},
+	watch: {
+	},
+	mounted() {
+	}
+}
+</script>
