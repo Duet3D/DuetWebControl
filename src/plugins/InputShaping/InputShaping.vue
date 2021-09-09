ea00ee7546bc6acb6dc545d8c6dc7ff977b8449e
Michael Szafranek <faule.aepfel@gmail.com>
2021-09-09 17:14:36 +0200

    InputShaping: updated comments and activated char component

--- src/plugins/InputShaping/InputShaping.vue
+++ src/plugins/InputShaping/InputShaping.vue
@@ -35,15 +35,14 @@
 				<session v-model="session"></session>
 			</v-tab-item>
 
-			<!-- recording -->
+			<!-- Recording -->
 			<v-tab-item value="recording" class="pa-3">
 				<recorder v-model="session"></recorder>
 			</v-tab-item>
 
 			<!-- Profile Analysis -->
 			<v-tab-item value="analysis" eager class="pa-3">
-				// TODO add chart here
-				<!--<chart></chart>-->
+				<chart v-model="session"></chart>
 			</v-tab-item>
 		</v-tabs-items>
 
