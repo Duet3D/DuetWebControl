33cd53867df5ff54bc10094791bfb8f425850e4f
Michael Szafranek <faule.aepfel@gmail.com>
2021-09-10 10:25:46 +0200

    InputShaping: added initial working chart component

--- src/plugins/InputShaping/Chart.vue
+++ src/plugins/InputShaping/Chart.vue
@@ -2,22 +2,418 @@
 </style>
 
 <template>
+	<v-row>
+		<v-col>
+		Chart Component
+		<v-card class="d-flex flex-column flex-grow-1 mt-2">
+			<v-card-title class="pt-2 pb-0">
+				<v-col>
+					<v-icon class="mr-1">mdi-chart-timeline-variant</v-icon> {{ $t('plugins.inputShaping.chartCaption') }}:
+
+					<v-list class="py-0">
+						<v-list-item-group :value="session.records" color="primary" mandatory>
+							<v-list-item v-for="record in session.records" :key="record.id" :value="record.name" @click="deleteRecord(name)">
+								{{ record.name }}
+							</v-list-item>
+						</v-list-item-group>
+					</v-list>
+				</v-col>
+				<v-col>
+					<v-icon class="mr-1">mdi-chart-timeline-variant</v-icon> {{ $t('plugins.inputShaping.filter') }}:
+
+					{{ checkedAxis }}
+					<v-container fluid>
+						<v-checkbox
+							v-for="axis in this.model.move.axes"
+							:key="axis.letter" :value=axis.letter :label="axis.letter"
+							v-model="checkedAxis"
+							hide-details
+						></v-checkbox>
+					</v-container>
+
+				</v-col>
+			</v-card-title>
+
+			<v-spacer></v-spacer>
+
+			<v-card-text class="pa-0" v-show="alertMessage !== null">
+				<v-alert :value="true" :type="alertType" class="mb-0">
+					{{ alertMessage  }}
+				</v-alert>
+			</v-card-text>
+
+			<v-spacer></v-spacer>
+
+			<v-card-text class="content flex-grow-1 px-2 py-0" @mousedown.passive="mouseDown" @mouseup.passive="mouseUp">
+				<canvas ref="chart" style="height: 400px;"></canvas>
+			</v-card-text>
+		</v-card>
+
+		<v-card class="mt-5" v-show="true">
+			<v-card-title>
+				<v-icon class="mr-2">mdi-calculator</v-icon> {{ $t('plugins.inputShaping.analysis') }}
+			</v-card-title>
+
+			<v-card-text>
+				<v-row>
+					<v-col>
+						<v-text-field v-model="samplingRate" :label="$t('plugins.inputShaping.samplingRate')" type="number" hide-details :disabled="!displaySamples"></v-text-field>
+					</v-col>
+					<v-col cols="auto">
+						<v-checkbox v-model="wideBand" :label="$t('plugins.inputShaping.wideBand')" hide-details :disabled="!displaySamples" class="mt-2"></v-checkbox>
+					</v-col>
+					<v-col cols="auto">
+						<v-btn v-show="true" color="primary" @click="updateChartFft">
+							<v-icon class="mr-2">mdi-arrow-right</v-icon> {{ $t('plugins.inputShaping.fft') }}
+						</v-btn>
+						<v-btn v-show="true" color="success" @click="updateChart">
+							<v-icon class="mr-2">mdi-arrow-left</v-icon> {{ $t('plugins.inputShaping.time') }}
+						</v-btn>
+						<v-btn v-show="true" color="success" @click="testChart">
+							TestChart
+						</v-btn>
+					</v-col>
+				</v-row>
+			</v-card-text>
+		</v-card>
+		</v-col>
+	</v-row>
 </template>
 
 <script>
 'use strict';
 
-import { AccelStates } from './InputShapingEnums.js';
-import { Record } from './InputShapingSession.js';
+import Chart from 'chart.js';
+
+import { mapState, mapGetters } from 'vuex';
 
 export default {
-	props: {
+	props: [ 'value' ],
+	data() {
+		return {
+			session: this.value,
+
+			wideBand: false,
+			recordList: [],
+			checkedAxis: [],
+
+			// obsolete?
+			loading: false,
+			alertType: 'error',
+			alertMessage: null,
+			displaySamples: false,
+			samplingRate: null,
+		}
 	},
 	computed: {
+		...mapState('settings', ['darkTheme']),
+		...mapState('machine', ['model']),
+		...mapGetters(['uiFrozen']),
 	},
 	watch: {
+		language() {
+			// TODO update chart
+			this.chart.options.scales.xAxes[0].scaleLabel.labelString = this.$t(this.displaySamples ? 'plugins.inputShaping.samples' : 'plugins.inputShaping.frequency');
+			this.chart.options.scales.yAxes[0].scaleLabel.labelString = this.$t(this.displaySamples ? 'plugins.inputShaping.accelerations' : 'plugins.inputShaping.amplitudes');
+			this.chart.update();
+		},
+		'session.records': {
+			handler() {
+			}
+		}
 	},
 	methods: {
+		toggleAxis(event, data) {
+			console.log(event, data);
+			console.log(this.checkedAxis);
+		},
+		getLineColor(index) {
+			const colors = [
+				'#4dc9f6',
+				'#f67019',
+				'#f53794',
+				'#537bc4',
+				'#acc236',
+				'#166a8f',
+				'#00a950',
+				'#58595b',
+				'#8549ba'
+			];
+			return colors[index % colors.length];
+		},
+		deleteRecord() {
+			this.session.removeRecord(name);
+			this.updateChart();
+		},
+		testChart() {
+
+			// chart update code works on record
+			let data = new Array(100);
+			for (let i = 0; i < data.length; i++) {
+				if (i == 0)
+					data[i] = 1;
+				else
+					data[i] = i + data[i - 1];
+			}
+
+			console.log("Test data", data);
+
+			// TODO is there a better source as sampling rate than a record
+			this.chart.data.datasets = [];
+
+			const dataset = {
+				hidden: false,
+				borderColor: this.getLineColor(0),
+				backgroundColor: this.getLineColor(0),
+				pointBorderWidth: 0.25,
+				pointRadius: 2,
+				borderWidth: 1.25,
+				data: data,
+				fill: false,
+				label: 'test data'
+			};
+
+			this.chart.data.datasets.push(dataset);
+
+			let labels = new Array(data.length);
+			for (let i = 0; i < labels.length; i++) {
+				labels[i] = i;
+			}
+
+			this.chart.data.labels = labels;
+			console.log("Test labels", this.chart.data.labels);
+
+			this.chart.options.tooltips.callbacks.title = items => this.$t('plugins.inputShaping.sampleTooltip', [items[0].index]);
+
+			this.chart.update();
+		},
+
+		updateChart() {
+
+			// chart update code works on record
+			let labels = new Array(this.session.test.param.numSamples);
+			for (let i = 0; i < labels.length; i++) {
+				labels[i] = i;
+			}
+			this.chart.data.labels = labels;
+			console.log("Acc labels", this.chart.data.labels);
+
+			this.chart.data.datasets = [];
+
+			this.session.records.forEach((rec, recIndex) => {
+
+				console.log("updating chart for", recIndex, rec);
+
+				// TODO is there a better source as sampling rate than a record
+				if (recIndex === 0)
+					this.samplingRate = rec.samplingRate;
+
+				rec.axis.forEach((axis, index, arr) => {
+					const dataset = {
+						hidden: this.checkedAxis.find(elem => elem === axis.name) < 0 ? true : false,
+						borderColor: this.getLineColor(index + recIndex * arr.length),
+						backgroundColor: this.getLineColor(index + recIndex * arr.length),
+						pointBorderWidth: 0.25,
+						pointRadius: 2,
+						borderWidth: 1.25,
+						data: axis.acceleration,
+						fill: false,
+						label: rec.name + ' ' + axis.name
+					};
+
+					this.chart.data.datasets.push(dataset);
+
+				});
+			});
+
+			this.chart.options.tooltips.callbacks.title = items => this.$t('plugins.inputShaping.sampleTooltip', [items[0].index]);
+
+			// Render the chart and apply sample rate
+			this.start = this.chart.config.options.scales.xAxes[0].ticks.min = 0;
+			this.end = this.chart.config.options.scales.xAxes[0].ticks.max = this.session.test.param.numSamples;
+
+			this.chart.update();
+
+			console.log("start", this.start, "end", this.end, "sampling rate", this.samplingRate);
+		},
+		updateChartFft() {
+
+			console.log("updateChartFft");
+
+			this.chart.data.labels = this.session.frequencies;
+			console.log("FFT labels", this.chart.data.labels);
+
+			this.chart.options.tooltips.callbacks.title = items => this.$t('plugins.inputShaping.frequencyTooltip', [this.session.frequencies[items[0].index].toFixed(2)]);
+
+			this.chart.data.datasets = [];
+
+			this.session.records.forEach((rec, recIndex) => {
+
+				console.log("updating chartfft for", recIndex, rec);
+
+				if (recIndex === 0)
+					this.samplingRate = rec.samplingRate;
+
+				rec.axis.forEach((axis, index, arr) => {
+					const dataset = {
+						hidden: this.checkedAxis.find(elem => elem === axis.name) < 0 ? true : false,
+						borderColor: this.getLineColor(index + recIndex * arr.length),
+						backgroundColor: this.getLineColor(index + recIndex * arr.length),
+						pointBorderWidth: 0.25,
+						pointRadius: 2,
+						borderWidth: 1.25,
+						data: axis.amplitudes,
+						fill: false,
+						label: rec.name + ' ' + axis.name
+					};
+
+					if (recIndex == 0)
+						console.log("name", rec.name, "acc", axis.acceleration, "amp", axis.amplitudes);
+
+					this.chart.data.datasets.push(dataset);
+				});
+			});
+
+			console.log("number of datasets complete", this.chart.data.datasets.length);
+			console.log("number of labels complete", this.chart.data.labels.length);
+
+			// Render the chart and apply sample rate
+			this.start = this.chart.config.options.scales.xAxes[0].ticks.min = 0;
+			this.end = this.chart.config.options.scales.xAxes[0].ticks.max = this.session.frequencies.length;
+
+			this.chart.update();
+		},
+		redraw() {
+			this.chart.update();
+		},
+		applyDarkTheme(active) {
+			const ticksColor = active ? '#FFF' : '#666';
+			this.chart.config.options.scales.xAxes[0].ticks.minor.fontColor = ticksColor;
+			this.chart.config.options.scales.xAxes[0].ticks.major.fontColor = ticksColor;
+			this.chart.config.options.scales.yAxes[0].ticks.major.fontColor = ticksColor;
+			this.chart.config.options.scales.yAxes[0].ticks.minor.fontColor = ticksColor;
+
+			const gridLineColor = active ? 'rgba(255,255,255,0.15)' : 'rgba(0,0,0,0.15)';
+			this.chart.config.options.scales.xAxes[0].gridLines.color = gridLineColor;
+			this.chart.config.options.scales.yAxes[0].gridLines.color = gridLineColor;
+			this.chart.config.options.scales.yAxes[0].gridLines.zeroLineColor = gridLineColor;
+
+			this.chart.update();
+		},
+		mouseDown(e) {
+			if (this.displaySamples) {
+				const activePoints = this.chart.getElementsAtEventForMode(e, 'nearest', { intersect: false });
+				this.dragStart = (activePoints && activePoints.length > 0) ? activePoints[0]._index : null;
+			}
+		},
+		mouseUp(e) {
+			const activePoints = this.chart.getElementsAtEventForMode(e, 'nearest', { intersect: false });
+			if (this.dragStart !== null && activePoints) {
+				const dragEnd = activePoints[0]._index;
+				if (this.dragStart != dragEnd) {
+					this.start = this.chart.config.options.scales.xAxes[0].ticks.min = Math.min(this.dragStart, dragEnd);
+					this.end = this.chart.config.options.scales.xAxes[0].ticks.max = Math.max(this.dragStart, dragEnd);
+					this.chart.update();
+				}
+				this.dragStart = null;
+			}
+
+			// $nextTick is too quick for chart.js
+			setTimeout((function() {
+				let datasetVisible = false;
+				for (let i = 0; i < this.chart.data.datasets.length; i++) {
+					if (this.chart.isDatasetVisible(i)) {
+						datasetVisible = true;
+						break;
+					}
+				}
+				this.datasetVisible = datasetVisible;
+			}).bind(this), 100);
+		},
+	},
+	mounted() {
+
+		// initially select all axis
+		this.checkedAxis = this.model.move.axes.map(e => e.letter);
+
+		// Prepare chart
+		const that = this;
+		this.options = {
+			maintainAspectRatio: false,
+			tooltips: {
+				mode: 'index',
+				intersect: false,
+				callbacks: {
+					title: items => that.$t('plugins.inputShaping.sampleTooltip', [items[0].index])
+				}
+			},
+			hover: {
+				mode: 'nearest',
+				intersect: true
+			},
+			scales: {
+				xAxes: [
+					{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: this.$t('plugins.inputShaping.samples')
+						},
+
+						gridLines: {
+							color: 'rgba(0,0,0,0.2)',
+							display: true
+						},
+						ticks: {
+							minor: {
+								fontColor: 'rgba(0,0,0,0.87)',
+								fontFamily: 'Roboto,sans-serif'
+							},
+							major: {
+								fontColor: 'rgba(0,0,0,0.87)',
+								fontFamily: 'Roboto,sans-serif'
+							},
+							beginAtZero: true
+						}
+					}
+				],
+				yAxes: [
+					{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: this.$t('plugins.inputShaping.accelerations')
+						},
+
+						gridLines: {
+							color: 'rgba(0,0,0,0.87)',
+							zeroLineColor: 'rgba(0,0,0,0.2)',
+							display: true
+						},
+						ticks: {
+							minor: {
+								fontColor: 'rgba(0,0,0,0.87)',
+								fontFamily: 'Roboto,sans-serif'
+							},
+							major: {
+								fontColor: 'rgba(0,0,0,0.87)',
+								fontFamily: 'Roboto,sans-serif'
+							}
+						}
+					}
+				]
+			}
+		};
+
+		// Create the chart
+		this.chart = Chart.Line(this.$refs.chart, {
+			options: this.options,
+			data: {
+				datasets: []
+			}
+		});
+		this.applyDarkTheme(this.darkTheme);
+
 	}
 }
 </script>
