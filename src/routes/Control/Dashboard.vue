8e0e7f77a25265d8135cf28d8b611becfb306012
Arron Bates <arron.bates@dentsu.com>
2021-10-01 18:15:10 -0400

    updating the internal/system page components to have keys rather than using the object structure, so that the configuration of the menu can use the keys as a placeholder for menu setup.

--- src/routes/Control/Dashboard.vue
+++ src/routes/Control/Dashboard.vue
@@ -31,13 +31,10 @@ export default {
 	install() {
 		// Register a route via Control -> Dashboard
 		registerRoute(this, {
-			Control: {
-				Dashboard: {
-					icon: 'mdi-view-dashboard',
-					caption: 'menu.control.dashboard',
-					path: '/'
-				}
-			}
+        key: 'control-dashboard',
+        icon: 'mdi-view-dashboard',
+        caption: 'menu.control.dashboard',
+        path: '/'
 		});
 	}
 };
