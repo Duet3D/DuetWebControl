89ae7b9bb069cb7c6de1d123eb3587ba0c669231
Christian Hammacher <chr.hammacher@gmail.com>
2023-06-06 13:34:53 +0200

    Improved version checks

--- src/components/dialogs/IncompatibleVersionsDialog.vue
+++ src/components/dialogs/IncompatibleVersionsDialog.vue
@@ -26,7 +26,7 @@
 </template>
 
 <script lang="ts">
-import semver from "semver";
+import semver, { patch } from "semver";
 import Vue from "vue";
 
 import packageInfo from "../../../package.json";
@@ -46,34 +46,48 @@ export default Vue.extend({
 	methods: {
 		checkVersions() {
 			if (store.state.machine.settings.checkVersions) {
-				let versionMismatch = false;
+				let versionMismatch = false, patchVersionMismatch = false;
 				try {
 					const mainboardVersion = store.state.machine.model.boards.find(board => !board.canAddress)?.firmwareVersion;
 					if (mainboardVersion) {
 						// Check expansion board firmware versions
 						for (const board of store.state.machine.model.boards) {
-							if (board.canAddress && board.firmwareVersion && semver.compare(mainboardVersion, board.firmwareVersion) !== 0) {
+							if (board.canAddress && board.firmwareVersion && semver.compare(mainboardVersion, board.firmwareVersion, true) !== 0) {
 								console.warn(`Expansion board #${board.canAddress} version mismatch (MB ${mainboardVersion} != EXP ${board.firmwareVersion})`);
-								versionMismatch = true;
+								if (semver.satisfies(board.firmwareVersion, '^' + mainboardVersion, true)) {
+									patchVersionMismatch = true;
+								} else {
+									versionMismatch = true;
+								}
 							}
 						}
 
 						// Check DSF version
-						if (!versionMismatch && store.state.machine.model.sbc !== null && semver.compare(mainboardVersion, store.state.machine.model.sbc.dsf.version) !== 0) {
+						if (!versionMismatch && store.state.machine.model.sbc !== null && semver.compare(mainboardVersion, store.state.machine.model.sbc.dsf.version, true) !== 0) {
 							console.warn(`DSF version mismatch (MB ${mainboardVersion} != DSF ${store.state.machine.model.sbc.dsf.version})`);
-							versionMismatch = true;
+							if (semver.satisfies(store.state.machine.model.sbc.dsf.version, '^' + mainboardVersion, true)) {
+								patchVersionMismatch = true;
+							} else {
+								versionMismatch = true;
+							}
 						}
 
 						// Check DWC version
-						if (!versionMismatch && semver.compare(mainboardVersion, packageInfo.version) !== 0) {
+						if (!versionMismatch && semver.compare(mainboardVersion, packageInfo.version, true) !== 0) {
 							console.warn(`DWC version mismatch (MB ${mainboardVersion} != DWC ${packageInfo.version})`);
-							versionMismatch = true;
+							if (semver.satisfies(packageInfo.version, '^' + mainboardVersion, true)) {
+								patchVersionMismatch = true;
+							} else {
+								versionMismatch = true;
+							}
 						}
 					}
 
 					this.shown = versionMismatch;
 					if (versionMismatch) {
-						this.$logToConsole(LogType.warning, this.$t("dialog.incompatibleVersions.title"), this.$t("dialog.incompatibleVersions.prompt"));
+						this.$logToConsole(LogType.error, this.$t("dialog.incompatibleVersions.title"), this.$t("dialog.incompatibleVersions.prompt"));
+					} else if (patchVersionMismatch) {
+						this.$log(LogType.warning, this.$t("dialog.incompatibleVersions.title"), this.$t("dialog.incompatibleVersions.prompt"));
 					}
 				} catch (e) {
 					console.warn("Failed to check software versions", e);
