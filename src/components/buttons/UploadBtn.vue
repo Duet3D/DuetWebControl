7dc80828373b71493593a5101592d8249e8404c6
Christian Hammacher <chr.hammacher@gmail.com>
2024-02-23 11:02:21 +0100

    Fixed detection of some WiFi module firmware files

--- src/components/buttons/UploadBtn.vue
+++ src/components/buttons/UploadBtn.vue
@@ -380,14 +380,18 @@ export default Vue.extend({
 							filename = Path.combine(store.state.machine.model.directories.firmware, iapFileNameSD);
 						} else if (!store.state.machine.model.sbc && store.state.machine.model.network.interfaces.some(iface => iface.type === NetworkInterfaceType.wifi)) {
 							if ((/DuetWiFiSocketServer(.*)\.bin/i.test(content.name) || /DuetWiFiServer(.*)\.bin/i.test(content.name))) {
+								// Deprecated; will be removed in v3.6
 								filename = Path.combine(store.state.machine.model.directories.firmware, "DuetWiFiServer.bin");
 								this.updates.wifiServer = true;
 							} else if (/DuetWebControl(.*)\.bin/i.test(content.name)) {
+								// Deprecated; will be removed in v3.6
 								filename = Path.combine(store.state.machine.model.directories.firmware, "DuetWebControl.bin");
 								this.updates.wifiServerSpiffs = true;
 							} else if (content.name.endsWith(".bin") || content.name.endsWith(".uf2")) {
 								filename = Path.combine(store.state.machine.model.directories.firmware, content.name);
-								if (content.name === "PanelDueFirmware.bin") {
+								if (store.state.machine.model.boards.some(board => board.wifiFirmwareFileName === content.name)) {
+									this.updates.wifiServer = true;
+								} else if (content.name === "PanelDueFirmware.bin") {
 									this.updates.panelDue = true;
 								}
 							}
