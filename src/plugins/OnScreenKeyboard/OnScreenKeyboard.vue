3c82a7e85f9a3821f135885bfd33390124a42ca5
Christian Hammacher <chr.hammacher@gmail.com>
2026-01-15 13:23:01 +0100

    Added decimal separators to OSK

--- src/plugins/OnScreenKeyboard/OnScreenKeyboard.vue
+++ src/plugins/OnScreenKeyboard/OnScreenKeyboard.vue
@@ -85,7 +85,7 @@ export default Vue.extend({
 									"{numpad1} {numpad2} {numpad3}",
 									"{numpad4} {numpad5} {numpad6}",
 									"{numpad7} {numpad8} {numpad9}",
-									"{bksp} {numpad0} {enter}"
+									"{bksp} . {numpad0} , {enter}"
 								]
 							}
 						});
